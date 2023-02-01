09ce6f589e30036ce69837941539b84008bd9980
Willian Batista <72418226+WillianBatista19@users.noreply.github.com>
2023-02-01 03:19:18 -0300

    atualizando redes sociais do footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -7,9 +7,9 @@ function Footer() {
                 <a className={styles.p_footer} href="https://github.com/WillianBatista19">Willian Batista</a>
                 <p className={styles.p_footer_follow}>Follow us:</p>
                 <a className={styles.logo_footer} href="https://github.com/WillianBatista19" target="blank"><img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" alt="Logo" /></a>
-                <a className={styles.logo_footer} href="https://github.com/WillianBatista19" target="blank"><img src="https://img.shields.io/badge/Instagram-E4405F?style=for-the-badge&logo=instagram&logoColor=white" alt="Logo" /></a> 
-                <a className={styles.logo_footer} href="https://github.com/WillianBatista19" target="blank"><img src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white" alt="Logo" /></a> 
-                <a className={styles.logo_footer} href="https://github.com/WillianBatista19" target="blank"><img src="https://img.shields.io/badge/Twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white" alt="Logo" /></a>                 
+                <a className={styles.logo_footer} href="https://www.instagram.com/natalnalua" target="blank"><img src="https://img.shields.io/badge/Instagram-E4405F?style=for-the-badge&logo=instagram&logoColor=white" alt="Logo" /></a> 
+                <a className={styles.logo_footer} href="https://www.linkedin.com/in/willian-alves-batista-b32b14206" target="blank"><img src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white" alt="Logo" /></a> 
+                <a className={styles.logo_footer} href="https://twitter.com/Natalnalua" target="blank"><img src="https://img.shields.io/badge/Twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white" alt="Logo" /></a>                 
             </div>
         </>
     )

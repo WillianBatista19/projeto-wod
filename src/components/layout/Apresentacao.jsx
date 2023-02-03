54423db4c097df4f490d9294cea11332d288c71b
Willian Batista <72418226+WillianBatista19@users.noreply.github.com>
2023-02-03 05:17:05 -0300

    primeiras implementações responsividade

--- src/components/layout/Apresentacao.jsx
+++ src/components/layout/Apresentacao.jsx
@@ -8,8 +8,8 @@ function Apresentacao() {
         <>
             <div className={styles.apresentacao}>
                 <div>
-                    <h1 className={styles.h1_apresentacao}>O que é RPG?</h1>
-                    <p className={styles.p_apresentacao}> <br />Do inglês, a sigla RPG significa “Role Playing Game", um jogo de interpretação de papéis. Indo muito mais além do que os clássicos jogos de tabuleiro, o jogador interpreta um personagem, levando em consideração suas emoções, personalidade e habilidades. Jogar RPG é muito mais que sentar em volta de uma mesa, é um hobby que estimula a imaginação, criatividade e desenvolvimento social.</p>
+                    <h2 className={styles.h2_apresentacao}>O que é RPG?</h2>
+                    <p className={styles.p_apresentacao}>Do inglês, a sigla RPG significa “Role Playing Game", um jogo de interpretação de papéis. Indo muito mais além do que os clássicos jogos de tabuleiro, o jogador interpreta um personagem, levando em consideração suas emoções, personalidade e habilidades. Jogar RPG é muito mais que sentar em volta de uma mesa, é um hobby que estimula a imaginação, criatividade e desenvolvimento social.</p>
                 </div>
                 <img className={styles.img_apresentacao} src={FundoLobo} alt="Lobisomem: O Apocalipse"/>
             </div>
@@ -17,15 +17,16 @@ function Apresentacao() {
             <div className={styles.apresentacao}>
                 <img className={styles.img_apresentacao_left} src={FundoVamp} alt="Vampiro: A Mascara"/>
                 <div>
-                    <h1 className={styles.h1_apresentacao}>O que é World of Darkness?</h1>
-                    <p className={styles.p_apresentacao}> <br />Mundo das Trevas é o nome dado a dois universos ficcionais relacionados, mas distintos, criados como configurações de campanha para role-playing games de horror originalmente da editora estadunidense White Wolf. O primeiro foi concebido por Mark Rein-Hagen, enquanto o segundo foi projetado por várias pessoas da White Wolf Gaming Studio, que Rein-Hagen ajudou a criar.</p>
+                    <h1 className={styles.h2_apresentacao}>O que é World of Darkness?</h1>
+                    <p className={styles.p_apresentacao}>Mundo das Trevas é o nome dado a dois universos ficcionais relacionados, mas distintos, criados como configurações de campanha para role-playing games de horror originalmente da editora estadunidense White Wolf. O primeiro foi concebido por Mark Rein-Hagen, enquanto o segundo foi projetado por várias pessoas da White Wolf Gaming Studio, que Rein-Hagen ajudou a criar.</p>
                 </div>
+                <img className={styles.img_apresentacao_left2} src={FundoVamp} alt="Vampiro: A Mascara"/>
             </div>
 
             <div className={styles.apresentacao}>
                 <div>
-                    <h1 className={styles.h1_apresentacao}>Sobre o Projeto</h1>
-                    <p className={styles.p_apresentacao}> <br />O projeto foi criado pela junção de dois fatores, o estudo e o amor pelo RPG. O site foi projetado para fins de estudo na área da programação, e nada melhor do que estudar enquanto espalha seu hobby favorito. O objetivo do site é apresentar o universo de WoD para novos jogadores e os ensinar a jogar (ou servir de consulta para veteranos). <a href="https://github.com/WillianBatista19">Saiba mais!</a></p>
+                    <h1 className={styles.h2_apresentacao}>Sobre o Projeto</h1>
+                    <p className={styles.p_apresentacao}>O projeto foi criado pela junção de dois fatores, o estudo e o amor pelo RPG. O site foi projetado para fins de estudo na área da programação, e nada melhor do que estudar enquanto espalha seu hobby favorito. O objetivo do site é apresentar o universo de WoD para novos jogadores e os ensinar a jogar (ou servir de consulta para veteranos). <a href="https://github.com/WillianBatista19">Saiba mais!</a></p>
                 </div>
                 <img className={styles.img_apresentacao} src={FundoMago} alt="Mago: A Ascensão"/>
             </div>
--- src/components/layout/Title.jsx
+++ src/components/layout/Title.jsx
@@ -2,8 +2,8 @@ import styles from '../Pages/Home/HomeCSS/home.module.css'
 
 function Title() {
     return (
-        <div>
-            <h2 className={styles.title}>WORLD OF </h2>
+        <div className={styles.titles}>
+            <h2 className={styles.title}>WORLD OF</h2>
             <h1 className={styles.title1}>DARKNESS</h1>
         </div>
         
